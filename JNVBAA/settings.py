bf89e67ef67b609054c42bd9647bd4ad000be737
Deepak Mandal <44017289+depak379mandal@users.noreply.github.com>
2020-07-22 08:01:14 +0530

    restructuring architecture

--- JNVBAA/settings.py
+++ JNVBAA/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'JNVBAA.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [os.path.join(BASE_DIR, "dist/templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
